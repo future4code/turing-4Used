0b159c3bbfe323734fa9b11713b3e47b6aed0639
Jailsom-Nogueira <jailsom.nogueira@gmail.com>
2020-07-07 20:46:03 -0300

    Footer estatico por Jailsom

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,11 @@
 import React, { Component } from 'react'
+import Footer from "./Footer/Index";
 
 export class AppContainer extends Component {
   render() {
     return (
       <div>
-
+        <Footer />
       </div>
     )
   }
---
+++ src/components/Footer/styles.js
@@ -0,0 +1,12 @@
+import styled from "styled-components";
+  
+ export const TextoFooter = styled.div`
+      display: flex;
+      justify-content: center;
+      align-items: center;
+      height: 40px;
+      color: white;
+      background-color: #00bcd5;
+      bottom: 0;
+      position: 'sticky';
+  `;
