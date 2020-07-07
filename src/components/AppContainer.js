1ae768a6e11210b3274aba4ee90877e4b67f3f7f
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-07-07 16:29:07 -0300

    Criação estática da seção de incluir produto e do filtro

--- src/components/AppContainer.js
+++ src/components/AppContainer.js
@@ -1,10 +1,14 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
+import ComponentPostarProduto from './ComponentPostarProduto';
+import ComponentFiltro from './ComponentFiltro';
 
 export class AppContainer extends Component {
   render() {
+    
     return (
       <div>
-
+        <ComponentFiltro />
+        <ComponentPostarProduto />
       </div>
     )
   }
