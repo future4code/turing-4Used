53d6711a3ad54b6c6a688e195eb40f9c2104c320
Hyago Ribeiro <hyagogabriel_r@hotmail.com>
2020-07-08 15:03:36 -0300

    adição dos onchange

--- src/components/ComponentFiltro.js
+++ src/components/ComponentFiltro.js
@@ -4,7 +4,6 @@ import styled from 'styled-components';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 
-
 const ContainerFiltro = styled.div `
     width: 750px;
     display: flex;
@@ -12,23 +11,49 @@ const ContainerFiltro = styled.div `
     margin: 0 auto;
 `;
 
+
+
 export class ComponentFiltro extends Component {
+  state = {
+    valorInputMin: "",
+    valorInputMax: "",
+    valorCategoria: "",
+  }
+
+  onChangeValorMin = (e) => {
+    this.setState({valorInputMin: e.target.value})
+    console.log(this.state.valorInputMin)
+  }
+
+  onChangeValorMax = (e) => {
+    this.setState({valorInputMax: e.target.value})
+    console.log(this.state.valorInputMax)
+  }
+
+  onChangeValorCategoria = (e) => {
+    this.setState({valorCategoria: e.target.value})
+    console.log(this.state.valorCategoria)
+  }
+
   render() {
     
     return (
       <div>
           <ContainerFiltro>
             <TextField
+                    onChange={this.onChangeValorMin}
                     id="valor-min"
                     label="Valor Mínimo"
                     variant="outlined"
                 />
                 <TextField
+                    onChange={this.onChangeValorMax}
                     id="valor-max"
                     label="Valor Máximo"
                     variant="outlined"
                 />
                 <TextField
+                    onChange={this.onChangeValorCategoria}
                     id="categoria"
                     label="Busca por categoria"
                     variant="outlined"
